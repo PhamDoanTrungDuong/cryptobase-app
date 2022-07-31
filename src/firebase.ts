2da03a7b0b2538c4fd24cd14f4541916293f2ca1
thienanga075 <pdtduong@gmail.com>
2022-07-31 22:12:02 +0700

    Initialize

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCqRP5EOv6pd6gdn5N9XyJ8qImvpLZkwSs",
+  authDomain: "cryptobase-app.firebaseapp.com",
+  projectId: "cryptobase-app",
+  storageBucket: "cryptobase-app.appspot.com",
+  messagingSenderId: "759513563094",
+  appId: "1:759513563094:web:96febecf3711d5b429ae77"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
