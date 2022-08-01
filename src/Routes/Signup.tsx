9d6b1134ee8e969a0c5afd32f45d25d9563518da
thienanga075 <pdtduong@gmail.com>
2022-08-01 22:42:28 +0700

    adjust

--- src/Routes/Signup.tsx
+++ src/Routes/Signup.tsx
@@ -17,6 +17,7 @@ const Signup: React.FC = () => {
     setErrors('')
     try {
       await signUp(email, password)
+	navigate('/');
     } catch (error: any) {
       setErrors(error.message)
     }
